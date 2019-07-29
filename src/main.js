bc8f316634146d9857921b2301d154d9a2bb5c3f
bom199669911996 <bom_19966991@163.com>
2019-07-29 15:55:40 +0800

    初始化仓库

--- src/main.js
+++ src/main.js
@@ -2,7 +2,28 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import Vuex from 'vuex'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import 'amfe-flexible/index.js'
 
+Vue.use(Vant)
+// Vue.use(Vuex)
+
+// const store = new Vuex.Store({
+// 是对象
+// 作用：声明数据
+// state中的数据是交给组件去使用的
+// 组件中使用数据 可以{{this.$store.state.count}}
+//   state: {
+//     count: 0
+//   },
+//   mutations: {
+//     increment (state) {
+//       state.count++
+//     }
+//   }
+// })
 Vue.config.productionTip = false
 
 new Vue({
